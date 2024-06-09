7e5310336f13cc73bec063359223c84c32a52b7f
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-06-09 14:37:55 +0530

    Initial Commit

---
+++ BarcodeScanner-iOS17/BarcodeScanner_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScanner_iOS17App.swift
+//  BarcodeScanner-iOS17
+//
+//  Created by Mayur Vaity on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScanner_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
